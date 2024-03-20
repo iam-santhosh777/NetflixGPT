18bc57a8dc321560aced08061fb1aff5b13d36f1
santhosh kumar <thisis.santhosh777@gmail.com>
2024-03-21 00:04:03 +0530

    SignIn & SignUp Form Created

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
     - Signin / Signup Form
     - redirect to Browse Page
     - Login Form 
+    - SignUp Form
 
 - Browse (after authentication)
     - Header
@@ -20,3 +21,21 @@
 - Netflix GPT
     - Search Bar
     - Movie Suggestions
+
+# know about z index and formik
+## Z-Index 
+Z-index is used to stack elements on a webpage. It determines the order in which elements overlap each other. An element with a higher z-index will be displayed in front of an 2D plane. It determines the order of 2D plane. It specifies the order of2D plane.
+
+## formik 
+
+- Formik is a comprehensive solution for forms validation with React. It has the following features:
+1. Handles form submission, including server  side submission or preventing it from submitting if invalid.
+2. Provides an easy way to manage form state that can be used to render your UI.
+3. Builds on standard HTML input elements to provide robustness and accessibility.
+4. Includes a schema validator for complex form structures.
+5. Supports custom validation via a global or per field basis.
+6. Allows for i18n of error messages.
+7. Works well with any component library by utilizing render props pattern.
+8. Can be used alongside Redux Form or other state management libraries.
+9. Is compatible with both controlled and un controlled components.
+10. Offers hook support for those using functional components.
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 const Login = () => {
+    const [isSignInForm, setIsSignInForm] = useState(true);
+;
+    const toggleSigninForm = () => {
+        setIsSignInForm(!isSignInForm);
+        
+    }
 
     return (
         <div>
             <Header />
             <div className='absolute h-full'>
-                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="netflixBg" className='relative h-full w-[100vw] bg-cover'/>
+                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="netflixBg" className='relative h-auto w-[100vw] bg-cover'/>
             </div>
-            <form className='flex flex-col w-full md:w-4/12 flex-shrink-0 absolute text-white bg-black bg-opacity-80 p-12 my-[12%] mx-auto right-0 left-0 rounded-lg'>
-                <h1 className='text-2xl font-medium py-2'>Sign In</h1>
-                
+            <form className='flex flex-col w-[50%] md:w-4/12 flex-shrink-0 absolute text-white bg-black bg-opacity-80 p-12 my-[12%] mx-auto right-0 left-0 rounded-lg'>
+                <h1 className='text-2xl font-medium py-2'>{isSignInForm ? 'Sign In' : 'Sign Up'}</h1>
+                {
+                    !isSignInForm && <input type='text' placeholder='Name' className='p-2 my-2 w-full bg-gray-950 bg-opacity-30 border border-zinc-400 rounded-md'></input>
+                }
                 <input type='text' placeholder='Email or phone number' className='p-2 my-2 w-full bg-gray-950 bg-opacity-30 border border-zinc-400 rounded-md'></input>
                 <input type='password' placeholder='Password' className='p-2 my-2 w-full bg-gray-950 bg-opacity-30 border border-zinc-400 rounded-md'></input>
-                <button type='submit' className='bg-red-700 p-2 my-2 w-full rounded-lg'>Sign In</button>
+                {
+                    !isSignInForm && <input type='password' placeholder='Confirm Password' className='p-2 my-2 w-full bg-gray-950 bg-opacity-30 border border-zinc-400 rounded-md'></input>
+                }
+                <button type='submit' className='bg-red-700 p-2 my-2 w-full rounded-lg'>{isSignInForm ? 'Sign In' : "Create an Account"}</button>
+                <p className='py-2'>{isSignInForm ? 'New to Netflix?' : 'Already have an account?'} <span onClick={toggleSigninForm} className='text-red-700 cursor-pointer'>{isSignInForm ? "Sign up now" : 'Sign In now'}</span></p>
 
             </form>
         </div>
